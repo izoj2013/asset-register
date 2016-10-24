7ee1f0411bf471551a910587838e44a4b32d39a3
izoj2013 <mivieux@yahoo.com>
2016-10-24 15:43:07 +0200

    AR First commit

---
+++ src/main/java/com/oisl/freesw/AssetRegisterApplication.java
@@ -0,0 +1,12 @@
+package com.oisl.freesw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssetRegisterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AssetRegisterApplication.class, args);
+	}
+}
---
+++ src/test/java/com/oisl/freesw/AssetRegisterApplicationTests.java
@@ -0,0 +1,16 @@
+package com.oisl.freesw;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AssetRegisterApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
